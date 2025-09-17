f1f97a0975fe5255f1edb50cebf132242acf917b
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-09-17 10:25:29 +0530

    Revert "work"
    This reverts commit 6e851c2a1a158c7408cf4de05b7ae84eae57116f.

--- backend/routes/geospatial.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAnnualRainfall } = require('../controllers/geospatial.controller');
-
-// Defines the endpoint: GET /api/geospatial/rainfall
-router.get('/rainfall', getAnnualRainfall);
-
-module.exports = router;
--- backend/routes/rainfall.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRainfallPrediction } = require('../controllers/rainfall.controller');
-
-router.post('/predict', getRainfallPrediction);
-
-module.exports = router;
